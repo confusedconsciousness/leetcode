978fd00a6a311a0c22e363c36b7fddaae50aa44d
Kishan Kumar <51268436+confusedconsciousness@users.noreply.github.com>
2024-01-06 22:28:34 +0530

    added one of the most mind boggling question, 'Minimum Number of Operations to Make X and Y Equal'

---
+++ src/medium/MinimumOperationsToMakeEqual.java
@@ -0,0 +1,113 @@
+package medium;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * You are given two positive integers x and y. <br/>
+ *
+ * In one operation, you can do one of the four following operations:<br/>
+ *
+ * Divide x by 11 if x is a multiple of 11.<br/>
+ * Divide x by 5 if x is a multiple of 5.<br/>
+ * Decrement x by 1.<br/>
+ * Increment x by 1.<br/>
+ * Return the minimum number of operations required to make x and y equal
+ */
+public class MinimumOperationsToMakeEqual {
+    Set<String> visited = new HashSet<>(); // this will prevent us from running indefinitely
+    static class Node {
+        private int x;
+        private int y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+        public int getY() {
+            return y;
+        }
+    }
+
+    public int minimumOperationsToMakeEqual(int x, int y) {
+        // if we think about this question, it will come out as a BFS.
+        // at any node we'll be having four options mentioned above
+        // each option will take us to a different node. We have to stop when we find the first node where x and y are equal
+        // make sure x and y are within bounds
+        // 1 <= x, y <= 10^4
+
+        Node seed = new Node(x, y);
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(seed);
+        queue.add(null); // this signifies that we have covered one depth
+
+        int operations = 0; // operation in other word will tell us how far we have come from the seed node
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+
+            // check if we have covered one depth, if yes increase the operation by 1
+            if (node == null) {
+                operations += 1;
+
+                // this means no other elements are left, so we should terminate the process
+                if (queue.isEmpty()) {
+                    return operations;
+                }
+                queue.add(null);
+                continue;
+            }
+
+            // if the node is already visited no need to put it in the queue again
+            if (visited.contains(getSignature(node))) {
+                continue;
+            }
+            visited.add(getSignature(node));
+
+            // base case
+            if (node.getX() == node.getY()) {
+                return operations;
+            }
+
+            // otherwise we'll have to create four different node based on our four different operations
+            Node child;
+            if (node.getX() % 11 == 0) {
+                child = new Node(node.getX() / 11, y);
+                queue.add(child);
+            }
+
+            if (node.getX() % 5 == 0) {
+                child = new Node(node.getX() / 5, y);
+                queue.add(child);
+            }
+
+            if (node.getX() < 9999) {
+                // otherwise we would create a node (10_0001, y) which is not inside the bounds
+                child = new Node(node.getX() + 1, y);
+                queue.add(child);
+            }
+
+            if (node.getX() > 1) {
+                // otherwise we would create a node (0, y) which is not inside the bounds
+                child = new Node(node.getX() - 1, y);
+                queue.add(child);
+            }
+        }
+
+        return operations;
+    }
+
+    private String getSignature(Node node) {
+        return node.getX() + ":" + node.getY();
+    }
+
+    public static void main(String[] args) {
+        MinimumOperationsToMakeEqual test = new MinimumOperationsToMakeEqual();
+        System.out.println(test.minimumOperationsToMakeEqual(25, 30));
+    }
+}
